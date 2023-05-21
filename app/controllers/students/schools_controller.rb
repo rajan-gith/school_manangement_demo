b30fcfae28502711733ea3dbf84f007b89129ef3
rajan-gith <rajan.beryls@gmail.com>
2023-05-21 13:16:07 +0530

    pagination added in project

--- app/controllers/students/schools_controller.rb
+++ app/controllers/students/schools_controller.rb
@@ -1,11 +1,11 @@
 class Students::SchoolsController < ApplicationController
   before_action :authenticate_student!
   def index
-    @schools = School.all
+    @schools = School.all.paginate(page: (params[:page]||1), per_page: PER_PAGE)
   end
   
   def batches
     @school = School.find(params[:id])
-    @batches = @school.batches
+    @batches = @school.batches.paginate(page: (params[:page]||1), per_page: PER_PAGE)
   end
 end
